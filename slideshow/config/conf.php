4c00825d8fe6a4534a2629b57c279deb2323cbe6
Ryan Ours <me@ryanours.com>
2011-07-27 17:10:22 -0400

    added slideshow pro installation directory "slideshow"

---
+++ slideshow/config/conf.php
@@ -0,0 +1,14 @@
+<?php
+
+	$interface = 'mysqli';
+	$host = 'slideshow.angelabeaulieuphoto.com';
+	$db = 'abp_slideshow';
+	$user = 'prettys13';
+	$pass = 'ama1985cc';
+
+	$pre = 'ssp_';
+
+	$socket = '';
+	$port = '';
+
+?>
